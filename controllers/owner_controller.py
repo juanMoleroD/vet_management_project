0a8adbc24b151e367c2163d80b5c6f8c0f0c4c07
juanMoleroD <juanldimeco@gmail.com>
2022-07-29 23:42:00 +0100

    added one_owner() route to owner controller

--- controllers/owner_controller.py
+++ controllers/owner_controller.py
@@ -1,10 +1,14 @@
 from flask import Flask, Blueprint, render_template, redirect
 from repositories import owner_repository
 
-owners_blueprint = Blueprint('owners', __name__)
+owners_blueprint = Blueprint("owners", __name__)
 
-@owners_blueprint.route('/owners')
+@owners_blueprint.route("/owners")
 def all_owners():
     all_owners = owner_repository.select_all()
     return render_template("owners/owners.html", all_owners=all_owners)
 
+@owners_blueprint.route("/owners/<id>")
+def owner(id):
+    owner = owner_repository.select(id)
+    return render_template("owners/owner.html", owner=owner)
