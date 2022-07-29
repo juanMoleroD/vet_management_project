901cd36a338e77ef5b09265de7057c434b9a2895
juanMoleroD <juanldimeco@gmail.com>
2022-07-29 15:16:50 +0100

    created vet save and select. Created console.py to test and seed data

---
+++ console.py
@@ -0,0 +1,13 @@
+from models.vet import Vet
+
+from repositories import vet_repository
+
+#vet_repository.delete_all()
+
+
+vet_1 = Vet("Ace Ventura")
+vet_repository.save(vet_1)
+
+vet_2 = Vet("Dr Dolitle")
+vet_repository.save(vet_2)
+
---
+++ repositories/vet_repository.py
@@ -0,0 +1,18 @@
+from db.run_sql import run_sql
+from models.vet import Vet
+
+def save(vet):
+    sql = "INSERT INTO vets (name) VALUES (%s) RETURNING id"
+    values = [vet.name]
+    result = run_sql(sql, values)
+    vet.id = result[0]["id"]
+
+def select(id):
+    sql = "SELECT * FROM vets WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)
+    vet = None
+    if result:
+        vet = Vet(result["name"], result["id"])
+    return vet
+
