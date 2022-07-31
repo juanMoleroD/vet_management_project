b98e191fea3d94d01e2c2f9ff4aa3d9ebb21ace4
juanMoleroD <juanldimeco@gmail.com>
2022-07-31 16:56:36 +0100

    appointment_controller: completed the view all appointments fuctionality, create new and view it. Had a huge bug due to the appointment select method using the appointment id for selecting animals.

--- controllers/appointment_controller.py
+++ controllers/appointment_controller.py
@@ -1,3 +1,4 @@
+import pdb
 from flask import Flask, Blueprint,request, redirect, render_template
 from repositories import animal_repository, appointment_repository
 from models.appointment import Appointment
@@ -18,3 +19,14 @@ def show_one_appointment(id):
 def get_new_form():
     all_animals = animal_repository.select_all()
     return render_template("appointments/new.html", animals=all_animals)
+
+@appointments_blueprint.route("/appointments", methods=["POST"])
+def create_appointment():
+
+    animal = animal_repository.select(request.form["animal_id"])
+    check_in = request.form["check_in"]
+    check_out = request.form["check_out"]
+    new_appointment = Appointment(animal, check_in, check_out)
+    appointment_repository.save(new_appointment)
+    return redirect("/appointments")
+
--- repositories/appointment_repository.py
+++ repositories/appointment_repository.py
@@ -1,3 +1,4 @@
+import pdb
 from db.run_sql import run_sql
 from models.appointment import Appointment
 from repositories import animal_repository
@@ -15,7 +16,7 @@ def select(id):
     appointment = None
     if results:
         result = results[0]
-        animal = animal_repository.select(result["id"])
+        animal = animal_repository.select(result["animal_id"])
         check_in = result["check_in"]
         check_out = result["check_out"]
         appointment = Appointment(animal, check_in, check_out, id)
@@ -26,7 +27,7 @@ def select_all():
     results = run_sql(sql)
     appointments = []
     for row in results:
-        animal = animal_repository.select(row["id"])
+        animal = animal_repository.select(row["animal_id"])
         check_in = row["check_in"]
         check_out = row["check_out"]
         id = row["id"]
